6c93f684b6e3423f2859ef82fa5dadf8bfdabc2e
navila-luna <navila@uchicago.edu>
2023-07-28 10:40:00 -0400

    having issues converting datetime to negative

--- Root/scripts/gen_report.py
+++ Root/scripts/gen_report.py
@@ -1,5 +1,5 @@
 from glob import glob
-import datetime
+from datetime import datetime
 """
 Create WEEKLY-REPORT-<DATE>.json for all the orgs and projects,
 and save then inside _data directory.
@@ -9,7 +9,7 @@ Also call report_posts.py module to create _posts reflecting the reports.
 
 from glob import glob
 import heapq
-import datetime
+from datetime import datetime
 import json
 import os
 import re
@@ -26,6 +26,7 @@ MONTHLY_MIN_DIFFERENCE = 27 # In Days
            Weeks refers to the number of files we want.  weeks we wa
     Returns a list of file names listed from most recent to least recent.
 """
+
 def get_metrics_files(project, weeks):
     # Get the latest two metrics for this project which are MIN_DIFFERENCE days apart
     re_metrics = re.compile(r"METRICS-\d{4}-\d{2}-\d{2}.json")
@@ -33,17 +34,20 @@ def get_metrics_files(project, weeks):
 
     for filename in os.listdir(project):
         if re_metrics.match(filename):
-            all_metrics.append(filename)
+            datestamp = datetime.strptime(filename,"METRICS-%Y-%m-%d.json")
+            # print("here", -1 * datestamp)
+            all_metrics.append((datestamp, filename))
     heapq.heapify(all_metrics)
     metric_files = len(all_metrics)
     if metric_files < 2 or metric_files < weeks:
         return False, {}, {}
     files_data = []
     for _ in range(weeks):
-        week = heapq.heappop(all_metrics)
+        _, week = heapq.heappop(all_metrics)
         with open(PATH_TO_MOCK_DATA + "/" + week, "r") as file:
             week_data = file.read()
-        files_data.append(week_data)
+            week_data_json = json.loads(week_data)
+        files_data.append(week_data_json)
     return files_data
 
 
@@ -69,13 +73,49 @@ def get_highlight_score(latest, previous):
     return modulo_flag, modulo_number
 
 
-# calculates the difference between this week/monthly versus last  week/monthly per metric
-def calculate_weekly_diff(files):
-    pass
 
-def calculate_we_sum(files):
-    pass
+
+github_metrics ={'datetime': 0, 'commits_count': 0, 'issues_count': 0,
+                    'open_issues_count': 0,'closed_issues_count': 0, 'pull_requests_count': 0, 'open_pull_requests_count': 0,
+                    'merged_pull_requests_count': 0, 'closed_pull_requests_count': 0, 'forks_count': 0, 
+                    'stargazers_count': 0, 'watchers_count': 0}
+
+# calculates the difference between this week/monthly versus last week/monthly per metric
+def calculate_weekly_diff(files):
+    if len(files) > 2:
+        return False, {}
+    latest, previous  = files
+    print(latest, previous)
+    weekly_difference = {'datetime': 0, 'commits_count': 0, 'issues_count': 0,
+                    'open_issues_count': 0,'closed_issues_count': 0, 'pull_requests_count': 0, 'open_pull_requests_count': 0,
+                    'merged_pull_requests_count': 0, 'closed_pull_requests_count': 0, 'forks_count': 0, 
+                    'stargazers_count': 0, 'watchers_count': 0 }
+    for metric in latest:
+        if metric != "datetime" and latest[metric] is not None:
+            print("In here", latest[metric], previous[metric])
+            weekly_difference[metric] = latest[metric] - previous[metric]
+    return weekly_difference
+
+# Calculates the total sum across each github metric from the lastest month. 
+def calculate_monthly_sum(files):
+    if len(files) < 4:
+        return False, {}
+    monthly_report = {}
+    for file in files:
+        for metric in file:
+            if metric != "datetime" or file[metric] is not None:
+                if metric not in monthly_report:
+                    monthly_report[metric] = file[metric]
+                else:
+                    monthly_report[metric] += file[metric]
+    return monthly_report
         
 weekly_files = get_metrics_files(PATH_TO_MOCK_DATA, 2)
+
 latest, previous = weekly_files
-print(latest, previous)
+print("hooo", latest, previous)
+github_metrics = [metric_name for metric_name in latest]
+
+# differences = calculate_weekly_diff(weekly_files)
+# print(differences)
+
