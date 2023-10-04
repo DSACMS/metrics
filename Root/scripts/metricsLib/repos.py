123ee6dafdd9cdd9c39093357e3fb566eb620ce6
Isaac Milarsky <krabs@tilde.team>
2023-10-04 13:08:06 -0500

    Add Repository class that holds metrics for the purpose of converting them to displayable markdown
    Signed-off-by: Isaac Milarsky <krabs@tilde.team>

--- Root/scripts/metricsLib/repos.py
+++ Root/scripts/metricsLib/repos.py
@@ -1,9 +1,19 @@
 import os
 import json
 import requests
+import datetime
 
 from .constants import *
 
 class Repository:
-    def __init__(self):
-        raise NotImplementedError
+    def __init__(self,repo_git_url,basic_info, advanced_info):
+        
+        self.name = repo_git
+        
+        #Use all raw dict items as attributes of the repo.
+        for field, metric in basic_info.items():
+            #self.field = metric
+            setattr(self,field,metric)
+        
+        for field, metric in advanced_info.items():
+            setattr(self,field,metric)
