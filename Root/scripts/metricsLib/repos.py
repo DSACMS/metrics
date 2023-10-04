37d5a2d19e4f638d088a588a55f297c495c845cc
Isaac Milarsky <krabs@tilde.team>
2023-10-04 16:50:02 -0500

    Looks like github graphql is going to be the source for most simple info
    Signed-off-by: Isaac Milarsky <krabs@tilde.team>

--- Root/scripts/metricsLib/repos.py
+++ Root/scripts/metricsLib/repos.py
@@ -5,30 +5,6 @@ import datetime
 
 from .constants import *
 
-#Simple metric that can be represented by a count or value.
-class SimpleMetric:
-    #Url format should be in the vein of 'https://api.github.com/repos/{owner}/{repo}/issues?state=all'
-    #then url.format(**data)
-    def __init__(self,endpoint_url,return_value,params = None,token = None):
-        self.return_value = return_value
-
-        self.url = endpoint_url.format(**params)
-        
-
-        if token:
-            self.headers = {"Authorization": f"bearer {TOKEN}"}
-        else:
-            self.headers = None
-    
-    def get_value(self):
-        if self.headers:
-            response = requests.post(self.url,self.headers)
-        else:
-            resposnse = requests.post(self.url)
-        
-        return json.loads(response.txt)[self.return_value]
-
-
 
 class Repository:
     def __init__(self,repo_git_url,basic_info, advanced_info):
