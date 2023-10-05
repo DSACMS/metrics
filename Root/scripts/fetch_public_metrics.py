a7ac9076e1371c3b014a67093e6adc542699899a
Isaac Milarsky <krabs@tilde.team>
2023-10-05 11:32:06 -0500

    define how a metric asks for a specific set of parameters. In the future we will need access to the repo id of relevant repos in augur
    Signed-off-by: Isaac Milarsky <krabs@tilde.team>

--- Root/scripts/fetch_public_metrics.py
+++ Root/scripts/fetch_public_metrics.py
@@ -5,6 +5,8 @@ import os
 import requests
 
 from metricsLib.constants import *
+from metricsLib.repos import Repository
+import re
 
 #TODO: Where is this file coming from and what is generating it? Is this just unfinished.
 #with open(os.path.join(BASE_PATH, "graphql_queries.graphql"), "r") as file:
@@ -39,7 +41,28 @@ def repo_data_to_json(repositories):
         repo_json = json.loads(repo)
         repos[repo] = repo_json
 
+def get_repo_owner_and_name(repo_http_url):
+    """ Gets the owner and repo from a url.
 
+        Args:
+            url: Github url
+
+        Returns:
+            Tuple of owner and repo. Or a tuple of None and None if the url is invalid.
+    """
+        
+    result = re.search(r"https?:\/\/github\.com\/([A-Za-z0-9 \- _]+)\/([A-Za-z0-9 \- _ \.]+)(.git)?\/?$", repo_http_url)
+
+    if not result:
+        return None, None
+
+    capturing_groups = result.groups()
+
+
+    owner = capturing_groups[0]
+    repo = capturing_groups[1]
+
+    return owner, repo
 """
 Purpose: Simplifies the format of the  json file to include only the desired metrics
 Input: Requires a repository name defined from graphql_queries that is in json format
@@ -77,7 +100,7 @@ def output_repository_info(repositories):
 """
 
 # Filter for DSACMS organization dataset
-original_organization_data = public_repo_data["data"]["organization"]["original"]["nodes"]
+#original_organization_data = public_repo_data["data"]["organization"]["original"]["nodes"]
 # print(original_organization_data)
 
 # Store  the name of repo and the counts for desired Github metrics
@@ -85,50 +108,70 @@ all_repo_metrics_info = {}
 # Holds an org name as key and all the metrics per repo in that org
 DATA_JSON = {}
 
-#  Capture the metric data  from DSACMS
+#  Capture the metric data  from all repos
 #  Returns a nested dictionary
-for repo in original_organization_data:
-    name = repo["name"]
-    repos_tracked.add(name)
-    repo_metric_info = output_repository_info(repo)
-    all_repo_metrics_info[name] = repo_metric_info
+for repo in ALL_REPOS:
+    #prepare all of the parameters needed for each metric.
+    owner, repo = get_repo_owner_and_name(repo)
+    needed_params = {
+        "repo" : repo,
+        "owner" : owner
+    }
+
+    repo_simple_metrics = {}
+    repo_advanced_metrics = {}
+    
+    #Get info from all metrics for each repo
+    for metric in SIMPLE_METRICS:
+
+        params = {}
+        #Get the parameter for this metric
+        for param in metric.needed_parameters:
+            params[param] = needed_params[param]
+        
+        repo_simple_metrics.update(metric.get_values(params))
+    #repo_metric_info = output_repository_info(repo)
+
+    repoInfo = Repository(repo,repo_simple_metrics,repo_advanced_metrics)
+    all_repo_metrics_info[repo] = repoInfo
 
 # print(all_repo_metrics_info)
-print(original_organization_data)
+print(all_repo_metrics_info)
 print(type(all_repo_metrics_info))
-DATA_JSON["DSACMS"] = all_repo_metrics_info
 
-# Update _metadata/projects_tracked.json
-"""
-    Updates the projects_tracked.json file to have all the Github metric data 
-    for the desired orgs and repos 
-"""
-# TODO Apply all the orgs and there assigned repos in projects tracked
-PROJECTS_TRACKED['orgs'] = ["DSACMS"]
-PROJECTS_TRACKED['Open Source Projects'] = {"DSACMS": list(repos_tracked)}
-
-#with open(os.path.join(BASE_PATH, PATH_TO_METADATA + "/" + "projects_tracked.json"), "w+") as f:
-#    json.dump(PROJECTS_TRACKED, f)
-
-"""
-  Create a new json file Labeled METRICS-DATESTAMP.json
-  Will create a new folder for the given repo and org if 
-  it currently does not exist.
-"""
-list_of_org_projects = PROJECTS_TRACKED['Open Source Projects']["DSACMS"]
-given_org_data = DATA_JSON["DSACMS"]
-print("given_org_data", given_org_data)
-for repo in list_of_org_projects:
-    repo_metric_data = given_org_data.get(repo)
-    print("repo_metric_data", repo_metric_data, "\n")
-
-    # creates directory if it doesn't exist for a given repo
-    owner_dir_path = "{}/{}".format(PATH_TO_METRICS_DATA, "DSACMS")
-    repo_dir_path = "{}/{}".format(owner_dir_path, repo)
-    os.makedirs(repo_dir_path, exist_ok=True)
-
-    #   # Save the json file with a timestamp
-    file_name = "METRICS-" + DATESTAMP + ".json"
-    with open(repo_dir_path + "/" + file_name, "w+") as f:
-        json.dump(repo_metric_data, f)
-    print("LOG: Saving", file_name, "for", repo)
+#DATA_JSON["DSACMS"] = all_repo_metrics_info
+#
+## Update _metadata/projects_tracked.json
+#"""
+#    Updates the projects_tracked.json file to have all the Github metric data 
+#    for the desired orgs and repos 
+#"""
+## TODO Apply all the orgs and there assigned repos in projects tracked
+#PROJECTS_TRACKED['orgs'] = ["DSACMS"]
+#PROJECTS_TRACKED['Open Source Projects'] = {"DSACMS": list(repos_tracked)}
+#
+##with open(os.path.join(BASE_PATH, PATH_TO_METADATA + "/" + "projects_tracked.json"), "w+") as f:
+##    json.dump(PROJECTS_TRACKED, f)
+#
+#"""
+#  Create a new json file Labeled METRICS-DATESTAMP.json
+#  Will create a new folder for the given repo and org if 
+#  it currently does not exist.
+#"""
+#list_of_org_projects = PROJECTS_TRACKED['Open Source Projects']["DSACMS"]
+#given_org_data = DATA_JSON["DSACMS"]
+#print("given_org_data", given_org_data)
+#for repo in list_of_org_projects:
+#    repo_metric_data = given_org_data.get(repo)
+#    print("repo_metric_data", repo_metric_data, "\n")
+#
+#    # creates directory if it doesn't exist for a given repo
+#    owner_dir_path = "{}/{}".format(PATH_TO_METRICS_DATA, "DSACMS")
+#    repo_dir_path = "{}/{}".format(owner_dir_path, repo)
+#    os.makedirs(repo_dir_path, exist_ok=True)
+#
+#    #   # Save the json file with a timestamp
+#    file_name = "METRICS-" + DATESTAMP + ".json"
+#    with open(repo_dir_path + "/" + file_name, "w+") as f:
+#        json.dump(repo_metric_data, f)
+#    print("LOG: Saving", file_name, "for", repo)
