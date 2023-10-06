eb2f1a08a70f5afd73bf21d69960a26912415244
natalialuzuriaga <nat.luzuriaga@gmail.com>
2023-10-06 09:57:25 -0700

    Created metrics website skeleton using Eleventy

---
+++ Root/app/site/_data/site.js
@@ -0,0 +1,20 @@
+const production = process.env.NODE_ENV === "production"
+
+const host = production ? process.env.SITE_HOST : "http://0.0.0.0:8080"
+
+// For modifying the <base> tag
+const baseurl = production ? "" : ""
+
+module.exports = {
+  name: "CMS Open Source Repo Metrics",
+  title: "CMS Open Source Repo Metrics",
+  description: "Website containing reports and visualizations on repositories in CMSgov github organization.",
+  type: "website",
+  baseurl,
+  url: `${host}${baseurl}`,
+  domain: (host || "").replace("https://", ""),
+  production,
+  robots: production,
+  locale: "en-US",
+  nav: [{ url: "/about/", label: "About" }],
+}
