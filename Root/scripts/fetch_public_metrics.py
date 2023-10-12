c2d74fba3b1664db73bd08e853f8fa52e1bb1d86
Isaac Milarsky <krabs@tilde.team>
2023-10-12 11:31:44 -0500

    enforce pep8
    Signed-off-by: Isaac Milarsky <krabs@tilde.team>

--- Root/scripts/fetch_public_metrics.py
+++ Root/scripts/fetch_public_metrics.py
@@ -32,7 +32,7 @@ def repo_data_to_json(repositories):
 
 
 # Filter for DSACMS organization dataset
-#original_organization_data = public_repo_data["data"]["organization"]["original"]["nodes"]
+# original_organization_data = public_repo_data["data"]["organization"]["original"]["nodes"]
 # print(original_organization_data)
 
 # Store  the name of repo and the counts for desired Github metrics
@@ -43,27 +43,27 @@ DATA_JSON = {}
 #  Capture the metric data  from all repos
 #  Returns a nested dictionary
 for repo in ALL_REPOS:
-    #prepare all of the parameters needed for each metric.
+    # prepare all of the parameters needed for each metric.
     needed_params = {
-        "repo" : repo.name,
-        "owner" : repo.repo_owner,
-        "repo_id" : repo.repo_id,
+        "repo": repo.name,
+        "owner": repo.repo_owner,
+        "repo_id": repo.repo_id,
         "repo_group_id": repo.repo_group_id
     }
 
     print(needed_params)
     metrics_results = {}
-    
-    #Get info from all metrics for each repo
+
+    # Get info from all metrics for each repo
     for metric in SIMPLE_METRICS:
 
         params = {}
-        #Get the parameter for this metric
+        # Get the parameter for this metric
         for param in metric.needed_parameters:
             params[param] = needed_params[param]
-        
+
         metrics_results.update(metric.get_values(params))
-    #repo_metric_info = output_repository_info(repo)
+    # repo_metric_info = output_repository_info(repo)
 
     repo.store_metrics(metrics_results)
     all_repo_metrics_info[repo.url] = repo
@@ -73,29 +73,29 @@ for info, obj in all_repo_metrics_info.items():
     print(obj.commits_count)
 print(type(all_repo_metrics_info))
 
-#DATA_JSON["DSACMS"] = all_repo_metrics_info
+# DATA_JSON["DSACMS"] = all_repo_metrics_info
 #
-## Update _metadata/projects_tracked.json
-#"""
-#    Updates the projects_tracked.json file to have all the Github metric data 
-#    for the desired orgs and repos 
-#"""
-## TODO Apply all the orgs and there assigned repos in projects tracked
-#PROJECTS_TRACKED['orgs'] = ["DSACMS"]
-#PROJECTS_TRACKED['Open Source Projects'] = {"DSACMS": list(repos_tracked)}
+# Update _metadata/projects_tracked.json
+# """
+#    Updates the projects_tracked.json file to have all the Github metric data
+#    for the desired orgs and repos
+# """
+# TODO Apply all the orgs and there assigned repos in projects tracked
+# PROJECTS_TRACKED['orgs'] = ["DSACMS"]
+# PROJECTS_TRACKED['Open Source Projects'] = {"DSACMS": list(repos_tracked)}
 #
-##with open(os.path.join(BASE_PATH, PATH_TO_METADATA + "/" + "projects_tracked.json"), "w+") as f:
-##    json.dump(PROJECTS_TRACKED, f)
+# with open(os.path.join(BASE_PATH, PATH_TO_METADATA + "/" + "projects_tracked.json"), "w+") as f:
+# json.dump(PROJECTS_TRACKED, f)
 #
-#"""
+# """
 #  Create a new json file Labeled METRICS-DATESTAMP.json
-#  Will create a new folder for the given repo and org if 
+#  Will create a new folder for the given repo and org if
 #  it currently does not exist.
-#"""
-#list_of_org_projects = PROJECTS_TRACKED['Open Source Projects']["DSACMS"]
-#given_org_data = DATA_JSON["DSACMS"]
-#print("given_org_data", given_org_data)
-#for repo in list_of_org_projects:
+# """
+# list_of_org_projects = PROJECTS_TRACKED['Open Source Projects']["DSACMS"]
+# given_org_data = DATA_JSON["DSACMS"]
+# print("given_org_data", given_org_data)
+# for repo in list_of_org_projects:
 #    repo_metric_data = given_org_data.get(repo)
 #    print("repo_metric_data", repo_metric_data, "\n")
 #
--- Root/scripts/metricsLib/constants.py
+++ Root/scripts/metricsLib/constants.py
@@ -1,4 +1,5 @@
-import datetime, os
+import datetime
+import os
 from .metrics import SimpleMetric, GraphqlMetric, RangeMetric
 from .repos import Repository
 import json
@@ -10,7 +11,7 @@ PATH_TO_METADATA = "_metadata"
 DATESTAMP = datetime.datetime.now().date().isoformat()
 TOKEN = os.getenv("GITHUB_TOKEN")
 
-#The general procedure is to execute all metrics against all repos and orgs
+# The general procedure is to execute all metrics against all repos and orgs
 
 # TODO: Create a read repos-to-include.txt
 with open(os.path.join(PATH_TO_METADATA, "projects_tracked.json"), "r") as file:
@@ -19,24 +20,24 @@ with open(os.path.join(PATH_TO_METADATA, "projects_tracked.json"), "r") as file:
 
 ALL_ORGS = tracking_file["orgs"]  # Track orgs and all its repos e.g. DSACMS
 
-repo_urls = [] # Track specific repositories e.g. ['dsacms.github.io']
+repo_urls = []  # Track specific repositories e.g. ['dsacms.github.io']
 
 for _, repo_list in tracking_file["Open Source Projects"].items():
     repo_urls.extend(repo_list)
 
 ALL_REPOS = []
-#Create repo objects
+# Create repo objects
 for repo_url in repo_urls:
-  repo_obj = Repository(repo_url)
-  ALL_REPOS.append(repo_obj)
+    repo_obj = Repository(repo_url)
+    ALL_REPOS.append(repo_obj)
 
 
 SIMPLE_METRICS = []
 
-#Weekly, monthly metrics.
+# Weekly, monthly metrics.
 PERIODIC_METRICS = []
 
-#Classification metrics
+# Classification metrics
 ADVANCED_METRICS = []
 
 githubGraphqlQuery = """
@@ -99,20 +100,20 @@ query ($repo: String!, $owner: String!) {
 """
 
 
-SIMPLE_METRICS.append(GraphqlMetric("githubGraphqlSimpleCounts",["repo","owner"],githubGraphqlQuery,
-            {"commits_count": ["data","repository","defaultBranchRef","target","history","totalCount"],
-            "issues_count": ["data","repository","issues","totalCount"],
-            "open_issues_count": ["data","repository","openIssues","totalCount"],
-            "closed_issues_count": ["data","repository","closedIssues","totalCount"],
-            "pull_requests_count": ["data","repository","pullRequests","totalCount"],
-            "open_pull_requests_count": ["data","repository","openPullRequests","totalCount"],
-            "merged_pull_requests_count": ["data","repository","mergedPullRequests","totalCount"],
-            "closed_pull_requests_count": ["data","repository","closedPullRequests","totalCount"],
-            "forks_count": ["data","repository","forkCount"],
-            "stargazers_count": ["data","repository","stargazerCount"],
-            "watchers_count": ["data","repository","watchers","totalCount"]
-            },token=TOKEN))
+SIMPLE_METRICS.append(GraphqlMetric("githubGraphqlSimpleCounts", ["repo", "owner"], githubGraphqlQuery,
+                                    {"commits_count": ["data", "repository", "defaultBranchRef", "target", "history", "totalCount"],
+                                     "issues_count": ["data", "repository", "issues", "totalCount"],
+                                     "open_issues_count": ["data", "repository", "openIssues", "totalCount"],
+                                     "closed_issues_count": ["data", "repository", "closedIssues", "totalCount"],
+                                     "pull_requests_count": ["data", "repository", "pullRequests", "totalCount"],
+                                     "open_pull_requests_count": ["data", "repository", "openPullRequests", "totalCount"],
+                                     "merged_pull_requests_count": ["data", "repository", "mergedPullRequests", "totalCount"],
+                                     "closed_pull_requests_count": ["data", "repository", "closedPullRequests", "totalCount"],
+                                     "forks_count": ["data", "repository", "forkCount"],
+                                     "stargazers_count": ["data", "repository", "stargazerCount"],
+                                     "watchers_count": ["data", "repository", "watchers", "totalCount"]
+                                     }, token=TOKEN))
 
 newContributorsofCommits = "https://ai.chaoss.io/api/unstable/repos/{repo_id}/pull-requests-merge-contributor-new?period={period}&begin_date={begin_date}&end_date={end_date}"
-PERIODIC_METRICS.append(RangeMetric("newContributorsofCommits", ["repo_id","period","begin_date","end_date"],newContributorsofCommits,
-  {"new_commit_contributor" : "count"} ))
+PERIODIC_METRICS.append(RangeMetric("newContributorsofCommits", ["repo_id", "period", "begin_date", "end_date"], newContributorsofCommits,
+                                    {"new_commit_contributor": "count"}))
