d70cef9c5699c1cf2ba8c27b347ff945926185ac
Isaac Milarsky <isaac.milarsky@cms.hhs.gov>
2023-10-18 15:27:28 -0500

    create class for GithubOrg
    Signed-off-by: Isaac Milarsky <isaac.milarsky@cms.hhs.gov>

--- scripts/metricsLib/orgs.py
+++ scripts/metricsLib/orgs.py
@@ -5,6 +5,27 @@ import requests
 from .constants import *
 
 
-class RepositoryOrg:
-    def __init__(self):
-        raise NotImplementedError
+class GithubOrg:
+    def __init__(self, organization_login):
+        self.login = organization_login
+
+        #Get the group id from augur
+        augur_util_endpoint = f"https://ai.chaoss.io/api/unstable/owner/repo_groups"
+
+        response = requests.post(augur_util_endpoint)
+
+        response = requests.post(augur_util_endpoint)
+        response_dict = json.loads(response.text)
+
+        try:
+            for group in response_dict:
+                if group["rg_name"] == self.login:
+                    self.repo_group_id = group["repo_group_id"]
+                    break
+        
+        except Exception as e:
+            self.repo_group_id = None
+
+    def store_metrics(self,info):
+        for field,metric in info.items():
+            setattr(self,field,metric)
