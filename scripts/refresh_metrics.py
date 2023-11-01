f2cd42c6e2b8f6faa172a3cfeda4cb6a23887038
Isaac Milarsky <isaac.milarsky@cms.hhs.gov>
2023-11-01 15:58:36 -0500

    clean up debug
    Signed-off-by: Isaac Milarsky <isaac.milarsky@cms.hhs.gov>

--- scripts/refresh_metrics.py
+++ scripts/refresh_metrics.py
@@ -5,6 +5,7 @@ from metricsLib.constants import BASE_PATH, PATH_TO_METADATA, PATH_TO_METRICS_DA
 from fetch_public_metrics import get_all_parsed_data
 from clear_old_metrics import clear_all_data
 
+os.umask(0)
 # TODO: Create a read repos-to-include.txt
 with open(os.path.join(PATH_TO_METADATA, "projects_tracked.json"), "r") as file:
     tracking_file = json.load(file)
@@ -18,11 +19,7 @@ for _, repo_list in tracking_file["Open Source Projects"].items():
 all_orgs, all_repos = get_all_parsed_data(tracking_file["orgs"], repo_urls)
 
 #Delete old data
-os.umask(0)
 clear_all_data()
-os.umask(0)
-print("!!!!!!!!!!!!!!!!!")
-print(os.getcwd())
 
 
 #Save all metrics to files
