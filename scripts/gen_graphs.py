05b011727eff21a4c3a4d590bbe38d4c29f5c292
Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
2023-11-13 15:52:39 -0600

    Get graph gen working
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>

--- scripts/gen_graphs.py
+++ scripts/gen_graphs.py
@@ -4,25 +4,28 @@ import os
 import re
 import pygal
 
-PATH = "_dataVis"
-os.chdir(PATH)
-
-
 def generate_repo_solid_guage_issue_graph(repos):
     for repo in repos:
         issues_guage = pygal.SolidGauge(inner_radius=0.70)
         percent_formatter = lambda x: '{:.10g}%'.format(x)
         issues_guage.value_formatter = percent_formatter
 
-        issues_guage.add('Open Issues', [{'value': repo.metric_data['open_issues_count'], 'max_value': repo.metric_data['issues_count']}])
+        if repo.metric_data['issues_count'] > 0:
+            issues_guage.add('Open Issues', [{'value': repo.metric_data['open_issues_count'], 'max_value': repo.metric_data['issues_count']}])
 
-        issues_guage.add('Open Pull Requests', [{'value': repo.metric_data['open_pull_requests_count'], 'max_value': repo.metric_data['pull_requests_count']}])
-        issues_guage.add(
-            'Closed and Merged Pull Requests', [
-                {'value': repo.metric_data['merged_pull_requests_count'], 'max_value': repo.metric_data['pull_requests_count']},
-                {'value': repo.metric_data['closed_pull_requests_count'], 'max_value': repo.metric_data['pull_requests_count']}])
+        if repo.metric_data['pull_requests_count'] > 0:
+            issues_guage.add('Open Pull Requests', [{'value': repo.metric_data['open_pull_requests_count'], 'max_value': repo.metric_data['pull_requests_count']}])
+            issues_guage.add(
+                'Closed and Merged Pull Requests', [
+                    {'value': repo.metric_data['merged_pull_requests_count'], 'max_value': repo.metric_data['pull_requests_count']},
+                    {'value': repo.metric_data['closed_pull_requests_count'], 'max_value': repo.metric_data['pull_requests_count']}])
         
-        issues_guage.render_to_file(repo.get_path_to_graph_data("issue_guage"))
+        with open(repo.get_path_to_graph_data("issue_guage"), "wb+") as file:
+            try:
+                file.write(issues_guage.render())
+            except ZeroDivisionError:
+                print(f"Repo {repo.name} has zero pull requests or issues!")
+        #issues_guage.render_to_file(repo.get_path_to_graph_data("issue_guage"))
 
 # TODO: Just get these metrics from augur instead of storing them in json.
 def genOverview():
