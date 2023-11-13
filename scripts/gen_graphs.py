23379cbe36620d0ed85e8e924d62eb6ad57300cf
Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
2023-11-13 16:28:25 -0600

    Fix percentages on solidGuage graph
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>

--- scripts/gen_graphs.py
+++ scripts/gen_graphs.py
@@ -10,21 +10,32 @@ def generate_repo_solid_guage_issue_graph(repos):
         percent_formatter = lambda x: '{:.10g}%'.format(x)
         issues_guage.value_formatter = percent_formatter
 
-        if repo.metric_data['issues_count'] > 0:
-            issues_guage.add('Open Issues', [{'value': repo.metric_data['open_issues_count'], 'max_value': repo.metric_data['issues_count']}])
+        try:
+            open_issue_percent = repo.metric_data['open_issues_count'] / repo.metric_data['issues_count']
+        except ZeroDivisionError:
+            open_issue_percent = 0
+        issues_guage.add('Open Issues', [{'value': open_issue_percent * 100, 'max_value': 100}])
+        
+        try:
+            open_pr_percent = repo.metric_data['open_pull_requests_count'] / repo.metric_data['pull_requests_count']
+            merged_pr_percent = repo.metric_data['merged_pull_requests_count'] / repo.metric_data['pull_requests_count']
+            closed_pr_percent = repo.metric_data['closed_pull_requests_count'] / repo.metric_data['pull_requests_count']
+        except ZeroDivisionError:
+            open_pr_percent = 0
+            merged_pr_percent = 0
+            closed_pr_percent = 0
 
-        if repo.metric_data['pull_requests_count'] > 0:
-            issues_guage.add('Open Pull Requests', [{'value': repo.metric_data['open_pull_requests_count'], 'max_value': repo.metric_data['pull_requests_count']}])
-            issues_guage.add(
-                'Closed and Merged Pull Requests', [
-                    {'value': repo.metric_data['merged_pull_requests_count'], 'max_value': repo.metric_data['pull_requests_count']},
-                    {'value': repo.metric_data['closed_pull_requests_count'], 'max_value': repo.metric_data['pull_requests_count']}])
+        issues_guage.add('Open Pull Requests', [{'value': open_pr_percent * 100, 'max_value': 100}])
+        issues_guage.add(
+            'Closed and Merged Pull Requests', [
+                {'value': merged_pr_percent * 100, 'max_value': 100},
+                {'value': closed_pr_percent * 100, 'max_value': 100}])
         
         with open(repo.get_path_to_graph_data("issue_guage"), "wb+") as file:
             try:
                 file.write(issues_guage.render())
             except ZeroDivisionError:
-                print(f"Repo {repo.name} has zero pull requests or issues!")
+                print(f"Repo {repo.name} has a division by zero error when trying to make graph")
         #issues_guage.render_to_file(repo.get_path_to_graph_data("issue_guage"))
 
 # TODO: Just get these metrics from augur instead of storing them in json.
