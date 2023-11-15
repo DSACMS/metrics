bf59e03e635fcf83be4f30bea2ad2c8310146a96
Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
2023-11-15 13:21:21 -0600

    graph linting
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>

--- scripts/gen_graphs.py
+++ scripts/gen_graphs.py
@@ -1,5 +1,6 @@
-import json
-import os
+"""
+Module to define methods to create pygals graphs
+"""
 import pygal
 
 def generate_all_graphs_for_repos(all_repos):
@@ -95,56 +96,48 @@ def generate_repo_solid_guage_issue_graph(repos):
         
         
 # TODO: Just get these metrics from augur instead of storing them in json.
-def genOverview():
-    treemap = pygal.Treemap()
-    treemap.title = 'DSACMS Project Overview Binary TreeMap'
 
-    for file in os.listdir():
-        try:
-            f = open(file)
-            data = json.load(f)
-            d = []
-            words = file.split("-METRICS")
-
-            for key in data:
-                if (data[key] != 0 and data[key] != None):
-                    d.append(data[key])
-            d.pop(0)
-
-            treemap.add(words[0], d)
-        except:
-            invalid = []
-            invalid.append(file)
-
-    treemap.render_to_file('overview.svg')
-
-
-def repoSpecific():
-    for file in os.listdir():
-        try:
-            treemap = pygal.Treemap()
-            f = open(file)
-            data = json.load(f)
-            del data["datetime"]
-
-            for key in data:
-                if (data[key] != 0 and data[key] != None):
-                    treemap.add(key, data[key])
-
-            words = file.split("-METRICS")
-            treemap.title = words[0] + " Binary TreeMap"
-            treemap.render_to_file(words[0] + "-graph.svg")
-            data.clear()
-
-        except:
-            invalid = []
-            invalid.append(file)
-
-
-def main():
-    genOverview()
-    repoSpecific()
-
-
-if __name__ == "__main__":
-    main()
+#def genOverview():
+#    treemap = pygal.Treemap()
+#    treemap.title = 'DSACMS Project Overview Binary TreeMap'
+#
+#    for file in os.listdir():
+#        try:
+#            f = open(file)
+#            data = json.load(f)
+#            d = []
+#            words = file.split("-METRICS")
+#
+#            for key in data:
+#                if (data[key] != 0 and data[key] != None):
+#                    d.append(data[key])
+#            d.pop(0)
+#
+#            treemap.add(words[0], d)
+#        except:
+#            invalid = []
+#            invalid.append(file)
+#
+#    treemap.render_to_file('overview.svg')
+#
+#
+#def repoSpecific():
+#    for file in os.listdir():
+#        try:
+#            treemap = pygal.Treemap()
+#            f = open(file)
+#            data = json.load(f)
+#            del data["datetime"]
+#
+#            for key in data:
+#                if (data[key] != 0 and data[key] != None):
+#                    treemap.add(key, data[key])
+#
+#            words = file.split("-METRICS")
+#            treemap.title = words[0] + " Binary TreeMap"
+#            treemap.render_to_file(words[0] + "-graph.svg")
+#            data.clear()
+#
+#        except:
+#            invalid = []
+#            invalid.append(file)
