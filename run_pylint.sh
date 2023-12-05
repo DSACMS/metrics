3c1f2b54c0bd16d88882029ddc4fd08bd9f90432
Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
2023-12-05 13:55:06 -0600

    syntax
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>

--- run_pylint.sh
+++ run_pylint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-pylint --output-format=json --init-hook="import sys; sys.path.append('scripts/')" scripts/*
+#pylint --output-format=json --init-hook="import sys; sys.path.append('scripts/')" scripts/*
+python scripts/run_pylint.py
 exit 0
--- scripts/tests/metrics_tests.py
+++ scripts/tests/metrics_tests.py
@@ -1,5 +1,19 @@
 import pytest
+import json
 from metricsLib import metrics_data_structures
+import subprocess
 
 def test_base_metric():
     raise NotImplementedError
+
+def run_pylint():
+    pylint_out = subprocess.run(['pylint', '--output-format=json', "--init-hook=\"import sys; sys.path.append('scripts/')\"", "scripts/*"],stdout = subprocess.PIPE,check=False)
+
+    pylint_list = json.loads(pylint_out.stdout)
+
+    for item in pylint_list:
+        item['annotation_level'] = item.pop('type')
+        item['end_line'] = item.pop('endLine')
+        item['start_line'] = item.pop('line')
+    
+    print(json.dumps(pylint_list))
