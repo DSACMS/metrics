01090d0f83152a2c2b89cbf89c97f52e8df04bbd
Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
2023-12-05 13:25:37 -0600

    syntax
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>

--- run_pylint.sh
+++ run_pylint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pylint --init-hook="import sys; sys.path.append('scripts/')" scripts/*
+pylint --output-format=json --init-hook="import sys; sys.path.append('scripts/')" scripts/*
 exit 0
