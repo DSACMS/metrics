36a6d1948ff35f5deb48f33bf30c9ee7b42d4b01
Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
2023-12-05 14:30:37 -0600

    add comments
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>

--- scripts/tests/metrics_tests.py
+++ scripts/tests/metrics_tests.py
@@ -7,17 +7,24 @@ def test_base_metric():
     raise NotImplementedError
 
 def run_pylint():
+    #Run pylint command and parse json
     pylint_out = subprocess.run(['pylint', '--output-format=json', "--init-hook=\"import sys; sys.path.append('scripts/')\"", "scripts/*"],stdout = subprocess.PIPE,check=False)
 
     pylint_list = json.loads(pylint_out.stdout)
 
+
+    #Parse pylint format into GitHub Checks Annotation format
     for item in pylint_list:
+        #Rename error type to annotation_level
         item['annotation_level'] = item.pop('type')
 
+        #translate pylint terms to github terms
         if item['annotation_level'] == "convention":
             item['annotation_level'] = 'notice'
         elif item['annotation_level'] == "refactor":
             item['annotation_level'] = 'warning'
+        
+        #if end fields are empty, make them the same as the start
         item['end_line'] = item.pop('endLine')
         if item['end_line'] is None:
             item['end_line'] = item['line']
@@ -29,6 +36,7 @@ def run_pylint():
             item['end_column'] = item['start_column']
         item['title'] = item.pop('symbol')
 
+        #Don't include columns if annotation spans multiple lines
         if item['start_line'] != item['end_line']:
             item.pop('start_column')
             item.pop('end_column')
