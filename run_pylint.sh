bdf50cdbaa956825b0f5bd39685caa056f36e979
Isaac Milarsky <isaac.milarsky@hhs.cms.gov>
2024-01-17 10:54:27 -0600

    remove unneeded files and python
    Signed-off-by: Isaac Milarsky <isaac.milarsky@hhs.cms.gov>

--- run_pylint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#pylint --output-format=json --init-hook="import sys; sys.path.append('scripts/')" scripts/*
-python scripts/run_pylint.py
-exit 0
--- scripts/run_pylint.py
+++
@@ -1,3 +0,0 @@
-from tests.metrics_tests import run_pylint
-
-run_pylint()
