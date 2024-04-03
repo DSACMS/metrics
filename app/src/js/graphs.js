2263116cc0dbc0b2732013ec53bd427b0a064df0
Natalia Luzuriaga <natalia.luzuriaga@cms.hhs.gov>
2024-04-03 15:46:38 -0700

    toggle buttons styled according to selected graph
    Signed-off-by: Natalia Luzuriaga <natalia.luzuriaga@cms.hhs.gov>

--- app/src/js/graphs.js
+++ app/src/js/graphs.js
@@ -1,10 +1,17 @@
 window.showGraph = function(graphId) {
-    const graphs = document.querySelectorAll('.graph');
+    const graphs = document.querySelectorAll(".graph");
+    
     graphs.forEach(graph => {
-      if (graph.id == graphId) {
+      const button = document.querySelector("#button-" + graph.id);
+
+      if (graph.id == graphId && button.classList.contains("usa-button--outline")) {
+        // Shows selected graph
         graph.style.display = 'block';
+        button.classList.remove("usa-button--outline");
       } else {
+        // Hides rest of graphs
         graph.style.display = 'none';
+        button.classList.add("usa-button--outline");
       }
     });
   };
