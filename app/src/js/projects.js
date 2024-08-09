66a2b8fcb4ee9df33ab514f4d51f008cc8605a66
Ricardo Zamora <rzgarcia27@gmail.com>
2024-08-09 11:19:58 -0700

    render project cards in js
    Signed-off-by: Ricardo Zamora <rzgarcia27@gmail.com>

--- app/src/js/projects.js
+++ app/src/js/projects.js
@@ -1,10 +1,144 @@
+import { reportHeadingTemplate, projectCardTemplate } from "./templates";
 const filterBox = document.getElementById("filter-input");
-const projectSections = document.querySelectorAll(".project_section");
-const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+let projectSections = document.querySelectorAll(".project_section");
 const projectsData = document.getElementById('metrics').textContent;
-const parsedProjectsData = JSON.parse(projectsData)
-const filtersContainer = document.querySelector('.filters-container')
+const orgsData = document.getElementById('org-data').textContent;
+const parsedOrgsData = JSON.parse(orgsData);
+const parsedProjectsData = JSON.parse(projectsData);
+const filtersContainer = document.querySelector('.filters-container');
+const templateDiv = document.getElementById('content-container');
 
+
+
+//SORT
+const sortDirection = document.getElementById('sort-direction')
+const sortSelection = document.getElementById('sort-selection')
+// let sortedByOrg = sortSelection.value == "organization" ? true : false;
+// console.log(sortDirection)
+// console.log(sortSelection.value)
+// console.log(sortedByOrg)
+// document.getElementById("sort-direction-form").hidden = true;
+
+// sortSelection.addEventListener('change', function() {
+//  sortedByOrg = sortSelection.value == "organization" ? true : false;
+// //  console.log('ete')
+// //  console.log(sortedByOrg)
+// //  console.log(sortSelection.value)
+//  document.getElementById("sort-direction-form").hidden = false;
+//  parsedProjectsData.sort((a, b) => a.value - b.value);
+// })
+
+document.addEventListener("DOMContentLoaded", () => {
+  projectSections = document.querySelectorAll(".project_section");
+});
+
+// console.log("before projects: ", projects)
+// sortSelection.addEventListener('change', e => {
+//   const selection = sortSelection.value;
+//   console.log(sortSelection.value)
+
+//   for (org in projects) {
+//     // const repoData = projects[org][repoIndex]
+//     if (selection === "maturity_model_tier")
+//       sortByTier(projects[org])
+//   }
+// })
+// console.log("after projects: ", projects)
+
+function sortByTier(data) {
+  data.sort((a, b) => {
+    const tierA = a.maturity_model_tier ? Number(a.maturity_model_tier) : Infinity;
+    const tierB = b.maturity_model_tier ? Number(b.maturity_model_tier) : Infinity;
+    return tierA - tierB;
+  });
+  // data.sort((a, b) => Number(a.maturity_model_tier) - Number(b.maturity_model_tier));
+}
+
+const orgCheckboxes = document.getElementById('organization-content').querySelectorAll('.usa-checkbox')
+const organizations = [...orgCheckboxes].map(checkbox => checkbox.textContent.trim());
+
+// SORT END
+
+// eleventyConfig.addFilter("findProjectsInOrg", function (array, value) {
+//   return array.filter((item) => item["owner"] === value)
+// })
+
+let projects = {};
+
+organizations.forEach(org => {
+  projects[org] = getProjectsInOrg(parsedProjectsData, org);
+})
+
+function findObject(array, value) {
+  return array.find((item) => item["name"] === value)
+}
+
+function getProjectsInOrg(array, value) {
+  return array.filter((item) => item["owner"] === value)
+}
+
+sortSelection.addEventListener('change', e => {
+  const selection = sortSelection.value;
+
+  for (const org in projects) {
+    // const repoData = projects[org][repoIndex]
+    if (selection === "maturity_model_tier")
+      sortByTier(projects[org])
+      createProjectCards()
+  }
+})
+
+function createProjectCards() {
+  templateDiv.innerHTML = ''
+  for (const org in projects) {
+    const orgProject = findObject(parsedOrgsData, org);
+    const heading = reportHeadingTemplate(orgProject);
+    const projectSectionsTemplate = document.createElement('div');
+    projectSectionsTemplate.className = 'project_section'
+    templateDiv.append(projectSectionsTemplate)
+  
+    const reportHeading = document.createElement('div');
+    reportHeading.className = "report_heading"
+    reportHeading.innerHTML = heading;
+    projectSectionsTemplate.appendChild(reportHeading)
+  
+    const projectCards = document.createElement('ul');
+    projectCards.className = "usa-card-group flex-align-stretch"
+  
+    projectSectionsTemplate.appendChild(projectCards)
+  
+  
+    for (const repoIndex in projects[org]) {
+      const repoData = projects[org][repoIndex]
+      const projectCard = document.createElement('li');
+      projectCard.className = 'usa-card project-card tablet:grid-col-12'
+      projectCard.id = repoData.name
+      projectCard.setAttribute('org-name', repoData.owner)
+      projectCard.innerHTML = projectCardTemplate(repoData)
+      // const selectedFiltersObject = updateFilters()
+      // if (!passesFilterCriteria(repoData, selectedFiltersObject)) {
+      //   projectCard.hidden = true
+      // }
+      projectCards.appendChild(projectCard);
+    }
+  }
+  updateFilters()
+  // updateHeadingVisibility();
+}
+
+createProjectCards()
+
+// Keep filters after navigating back
+window.addEventListener('pageshow', (event) => {
+  updateFilters();
+
+  if (event.persisted) {
+    // The page was loaded from the bfcache (back-forward cache)
+    updateFilters();
+  }
+});
+
+// Checks for Checkbox event and updates filters
 addGlobalEventListener('change', '.usa-checkbox__input', e => {
   // Can use this e.target.name to update selected filters object
   updateFilters();
@@ -45,6 +179,8 @@ function updateFilters() {
   })
 
   updateHeadingVisibility();
+
+  // return selectedFiltersObject
 }
 
 // Function to add filters buttons
@@ -124,7 +260,6 @@ function updateHeadingVisibility() {
 // Function to return whether all filter criteria were met
 function checkFilterCriteria(card, selectedFiltersObject) {
   const cardName = card.querySelector(".text-no-underline").textContent;
-  // return array.filter((item) => item["owner"] === value)
   const currentProject = parsedProjectsData.find((project) => project["name"] === cardName)
   
   const matchesOrganization = selectedFiltersObject.organization.length === 0 || selectedFiltersObject.organization.includes(currentProject.owner);
@@ -137,6 +272,16 @@ function checkFilterCriteria(card, selectedFiltersObject) {
   card.hidden = !(matchesOrganization && matchesMaturityModelTier && matchesFismaLevel && matchesProjectType);
 
 }
+
+// function passesFilterCriteria(data, selectedFiltersObject) {
+//   const matchesOrganization = selectedFiltersObject.organization.length === 0 || selectedFiltersObject.organization.includes(data.owner);
+//   const projectMaturityModelTier = "Tier " + data.maturity_model_tier;
+//   const matchesMaturityModelTier = selectedFiltersObject.maturityModelTier.length === 0 || selectedFiltersObject.maturityModelTier.includes(projectMaturityModelTier);
+//   const matchesFismaLevel = selectedFiltersObject.fismaLevel.length === 0 || selectedFiltersObject.fismaLevel.includes(data.project_fisma_level);
+//   const matchesProjectType = selectedFiltersObject.projectType.length === 0 || selectedFiltersObject.projectType.includes(data.project_type);
+
+//   return (matchesOrganization && matchesMaturityModelTier && matchesFismaLevel && matchesProjectType);
+// }
 
 filterBox.addEventListener("input", () => {
   const query = filterBox.value.toLowerCase()
