963c656d9ae5adb4222241033bf9f686a4dd66e8
Dinne Kopelevich <dinne.kopelevich@gsa.gov>
2024-11-29 08:58:59 -0700

    Reafactor home main
    Signed-off-by: Dinne Kopelevich <dinne.kopelevich@gsa.gov>

--- app/src/js/nav.js
+++ app/src/js/nav.js
@@ -1,19 +1,3 @@
-// export function createNavigation() {
-//     const toggleButton = document.querySelector(".usa-menu-btn");
-//     const menu = document.querySelector(".usa-nav");
-//     const menuIcon = document.querySelector(".menu-icon use");
-
-//     toggleButton.addEventListener("click", () => {
-//         const isOpen = menu.classList.toggle("is-open");
-//         menuIcon.setAttribute(
-//             "xlink:href",
-//             isOpen ? "/assets/img/sprite.svg#close" : "/assets/img/sprite.svg#menu"
-//         );
-//     });
-// }
-
-// document.addEventListener("DOMContentLoaded", createNavigation);
-
 export function createNavigation() {
     const toggleButton = document.querySelector(".usa-accordion__button");
     const closeButton = document.querySelector(".usa-nav__close");
@@ -29,6 +13,12 @@ export function createNavigation() {
                 "xlink:href",
                 isOpen ? "/assets/img/sprite.svg#close" : "/assets/img/sprite.svg#menu"
             );
+        });
+
+        closeButton.addEventListener("click", () => {
+            menu.classList.remove("is-hidden");
+
+            menuIcon.setAttribute("xlink:href", "/assets/img/sprite.svg#menu");
         });
     } else {
         console.error("One or more elements is not found")
