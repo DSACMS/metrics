65de97600722ad1226798b3d807209243c81ea20
Dinne Kopelevich <dinne.kopelevich@gsa.gov>
2025-02-17 10:13:33 -0700

    Add open/close logic to filter containers
    Signed-off-by: Dinne Kopelevich <dinne.kopelevich@gsa.gov>

--- app/src/js/projects.js
+++ app/src/js/projects.js
@@ -36,6 +36,51 @@ sortDirection.addEventListener('change', () => {
   sortCards(isDescending);
 })
 
+// Controls filter menus open/closed state
+document.addEventListener("DOMContentLoaded", () => {
+  function updateFilterMenuState() {
+    const filterButtons = document.querySelectorAll(".usa-accordion__button");
+    const isMobile = window.innerWidth < 768;
+
+    filterButtons.forEach((button) => {
+      const contentId = button.getAttribute("aria-controls");
+      const content = document.getElementById(contentId);
+      const icon = button.querySelector("svg use");
+
+      if(isMobile) {
+        button.setAttribute("aria-expanded", "false");
+        content.setAttribute("hidden", "true");
+        icon.setAttribute("href", "/assets/img/sprite.svg#expand_more");
+      } else {
+        button.setAttribute("aria-expanded", "true");
+        content.removeAttribute("hidden");
+        icon.setAttribute("href", "/assets/img/sprite.svg#expand_less");
+      }
+    });
+  }
+
+  updateFilterMenuState()
+
+  window.addEventListener("resize", updateFilterMenuState)
+
+  document.querySelectorAll(".usa-accordion__button").forEach((button) => {
+    button.addEventListener("click", () => {
+      const expanded = button.getAttribute("aria-expanded");
+      const content = document.getElementById(button.getAttribute("id"));
+      const icon = button.querySelector("svg use");
+      
+      if(expanded === 'false') {
+        content.removeAttribute("hidden");
+        icon.setAttribute("href", "/assets/img/sprite.svg#expand_less");
+      } else {
+        content.setAttribute("hidden", "true");
+        icon.setAttribute("href", "/assets/img/sprite.svg#expand_more");
+      }
+    });
+  });
+});
+
+
 // Keep filters after navigating back
 window.addEventListener('pageshow', (event) => {
   updateFilters();
